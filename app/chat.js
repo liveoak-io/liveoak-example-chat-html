b6483ab379334965af2f5febf887afb604f89503
Ken Finnigan <ken@kenfinnigan.me>
2014-04-15 18:23:22 -0500

    Update HTML Chat App to handle correct removal and updating of messages based on event type

--- app/chat.js
+++ app/chat.js
@@ -5,17 +5,38 @@ $( function() {
 
   function add_message(data) {
     $( '#messages' ).append( 
-      $( '<div class="message">' ).append( 
+      $( '<div class="message" id="' + get_id( data ) + '">' ).append( 
         $('<div class="name">').append( data.name ) ).append( 
         $('<div class="text">').append( data.text ) ) );
     $( '#messages' ).scrollTop( $('#messages')[0].scrollHeight );
   }
 
+  function remove_message(data) {
+    $( '#' + get_id( data ) ).remove();
+  }
+
+  function update_message(data) {
+    var msg = $( '#' + get_id( data ) );
+    msg.find( '.name' ).html( data.name );
+    msg.find( '.text' ).html( data.text );
+  }
+
+  function get_id(data) {
+// Strip out everything before ':'
+    return data.id.substring(data.id.indexOf(':') + 1);
+  }
+
   liveoak.connect( function() {
     liveoak.create( '/chat-html/storage', { id: 'chat' }, {
       success: function(data) {
-        liveoak.subscribe( '/chat-html/storage/chat/*', function(data) {
-          add_message( data );
+        liveoak.subscribe( '/chat-html/storage/chat/*', function(data, action) {
+          if (action == 'create') {
+            add_message( data );
+          } else if (action == 'update') {
+            update_message( data );
+          } else if (action == 'delete') {
+            remove_message( data );
+          }
         } );
         liveoak.read( '/chat-html/storage/chat?expand=members', {
           success: function(data) {
